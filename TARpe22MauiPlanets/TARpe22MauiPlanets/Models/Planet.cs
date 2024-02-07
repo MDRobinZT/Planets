cf66d6bc253fcf9a5d262198b1c984ad10c23a6a
Opilane TTHK <opilane@tthk.ee>
2024-02-07 12:32:00 +0200

    Initial commit

---
+++ TARpe22MauiPlanets/TARpe22MauiPlanets/Models/Planet.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpe22MauiPlanets.Models
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public string Subtitle { get; set; }
+        public string HeroImage { get; set; }
+        public string Description { get; set; }
+        public Color AccentColorStart { get; set; }
+        public Color AccentColorEnd { get; set; }
+        public List<string> Images { get; set; }
+        public Brush Backround
+        {
+            get
+            {
+                var gradientStops = new GradientStopCollection();
+                gradientStops.Add(new GardientStop(AccentColorStart, 0.0f));
+                gradientStops.Add(new GardientStop(AccentColorStart, 1.0f));
+
+                var bgBrush = new LinearGradientBrush(
+                    gradientStops,
+                    new Point(0.0, 0.0),
+                    new Point(1.0, 1.0));
+
+                return bgBrush;
+
+            }
+        }
+    }
+}
